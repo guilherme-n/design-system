3e8110c033333a39edf42dbe2db659582b23815a
Guilherme Nascimento <guilherme.rdnascimento@gmail.com>
2022-10-17 12:08:08 -0300

    create storybook build

---
+++ storybook-static/assets/preview.f4a8799c.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.ee5672ab.js";import"./iframe.e4ec4e3c.js";import"./es.map.constructor.cac0df3a.js";import"./es.number.to-fixed.d9ee2461.js";import"./index.17281947.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.f4a8799c.js.map
